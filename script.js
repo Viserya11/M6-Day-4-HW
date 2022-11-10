599bb63d8137b965224c4f041bbfe7aca8832701
Panna Szűcs <szucspann@gmail.com>
2022-11-10 16:03:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-let object = {
-    "_id": "5d318e1a8541744830bef139", //SERVER GENERATED
-    "name": "app test 1",  //REQUIRED
-    "description": "somthing longer", //REQUIRED
-    "brand": "nokia", //REQUIRED
-    "imageUrl": "https://drop.ndtv.com/TECH/product_database/images/2152017124957PM_635_nokia_3310.jpeg?downsize=*:420&output-quality=80", //REQUIRED
-    "price": 100, //REQUIRED
-    "userId": "admin", //SERVER GENERATED
-    "createdAt": "2019-07-19T09:32:10.535Z", //SERVER GENERATED
-    "updatedAt": "2019-07-19T09:32:10.535Z", //SERVER GENERATED
-    "__v": 0 //SERVER GENERATED
-  }
+const options = {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZmUyZmQ0YmUzZDAwMTU4NDYwNDkiLCJpYXQiOjE2NjgwODczNDMsImV4cCI6MTY2OTI5Njk0M30.f0hqMCGmxQuJWfmA5JwepuldqFJL51QtNp2M1aDnsSE",
+    },
+};
 
-  console.log(JSON.stringify(object))
