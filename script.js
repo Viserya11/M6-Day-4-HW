b8cf127b40c7750ea0c945a8138b6a0ecf6ccdb1
Panna Szűcs <szucspann@gmail.com>
2022-11-10 16:59:47 +0100

    display

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const options = {
     method: "GET",
     headers: {
@@ -6,10 +8,48 @@ const options = {
     },
 };
 
-let display = async () => {
+const display = async () => {
     const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", options);
     const parsedBody = await response.json();
-    console.log(parsedBody)
-}
+    return parsedBody
+    };
 
-display()
+  
+ 
+
+    window.addEventListener("load", async () => {
+
+        const results = await display()
+
+        showData(results)
+         
+
+    })
+
+    const showData = (data) => {
+
+        let container = document.querySelector(".container")
+        container.innerHTML = ""
+
+        for (let product of data) {
+            container.innerHTML += `
+            
+            <div class="card">
+      <img src=${product.imageUrl} class="card-img-top" style="max-height: 25rem" alt="...">
+      <div class="card-body" style="height: 10rem">
+        <p class="card-text"> ${product.brand} </p>
+        <h3 class="card-text"> ${product.name} </h3>
+        <p class="card-text"> ${product.description} </p>
+        <p class="card-text"> ${product.price} </p>
+        <button href="#"  class="btn btn-primary">Buy</button>
+        <a href="backend.html"  class="btn btn-primary">Details</a>
+      </div>
+    </div>;
+
+
+
+            `
+        }
+
+
+    }
